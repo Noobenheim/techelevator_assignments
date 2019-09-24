fa263da13d4c16aa37453e1d25eb8353120b2c2c
Dan Podobnik <daniel.podobnik@gmail.com>
2019-09-24 12:47:58 -0400

    Completed Lecture.

--- module-1/07_Collections_Part_1/student-lecture/java/src/main/java/com/techelevator/Lecture.java
+++ module-1/07_Collections_Part_1/student-lecture/java/src/main/java/com/techelevator/Lecture.java
@@ -1,11 +1,9 @@
 package com.techelevator;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.Set;
 import java.util.Stack;
 
 public class Lecture {
@@ -14,30 +12,71 @@ public class Lecture {
 		System.out.println("####################");
 		System.out.println("       LISTS");
 		System.out.println("####################");
+		
+		List<String> names = new ArrayList<String>();
+		names.add("Happy");
+		names.add("Sleepy");
+		names.add("Grumpy");
+		names.add("Dopey");
+		names.add("Doc");
 
 		System.out.println("####################");
 		System.out.println("Lists are ordered");
 		System.out.println("####################");
+		
+		for( int i=0; i<names.size(); i++ ) {
+			String thisElement = names.get(i);
+			
+			System.out.println(thisElement);
+		}
 
 		System.out.println("####################");
 		System.out.println("Lists allow duplicates");
 		System.out.println("####################");
-
+		
+		names.add("Sleepy");
+		
+		for( int i=0; i<names.size(); i++ ) {
+			System.out.println(names.get(i));
+		}
+		
 		System.out.println("####################");
 		System.out.println("Lists allow elements to be inserted in the middle");
 		System.out.println("####################");
+		
+		names.add(4, "Tom");
+
+		for( int i=0; i<names.size(); i++ ) {
+			System.out.println(names.get(i));
+		}
 
 		System.out.println("####################");
 		System.out.println("Lists allow elements to be removed by index");
 		System.out.println("####################");
+		
+		names.remove(4);
+		
+		for( int i=0; i<names.size(); i++ ) {
+			System.out.println(names.get(i));
+		}
 
 		System.out.println("####################");
 		System.out.println("Find out if something is already in the List");
 		System.out.println("####################");
+		
+		boolean exists = names.contains("Tom");
+		
+		System.out.println(exists);
 
 		System.out.println("####################");
 		System.out.println("Lists can be turned into an array");
 		System.out.println("####################");
+		
+		String[] arrayOfStrings = names.toArray(new String[names.size()]);
+		
+		for( int i=0; i<arrayOfStrings.length; i++ ) {
+			System.out.println(arrayOfStrings[i]);
+		}
 
 		System.out.println("####################");
 		System.out.println(" PRIMITIVE WRAPPERS");
@@ -45,16 +84,42 @@ public class Lecture {
 
 		/* Every primitive data type has an equivalent "primitive wrapper class" that is an object representation
 		 * of a primitive value */
+		
+		List<Integer> scores = new ArrayList<Integer>();
+		int numScores = ((int)(Math.random()*10)) + 10;
+		
+		for( int i=0; i < numScores; i++ ) {
+			int score = (int)Math.round((Math.random()*50)+50);
+			scores.add(score);
+		}
+		
+		System.out.println(scores);
 
 		System.out.println("####################");
 		System.out.println("       FOREACH");
 		System.out.println("####################");
 		System.out.println();
+		
+		for( int i : scores ) {
+			System.out.println(i);
+		}
 
 		System.out.println("####################");
 		System.out.println("       QUEUES");
 		System.out.println("####################");
 		System.out.println();
+		
+		Queue<String> priorities = new LinkedList<String>();
+		priorities.offer("Clean the dishes");
+		priorities.offer("Wash the counters");
+		priorities.offer("Sweep the leg");
+		priorities.offer("Scrub the floor");
+		
+		while( !priorities.isEmpty() ) {
+			String firstPriority = priorities.poll();
+			
+			System.out.println("Your current priority is "+firstPriority);
+		}
 
 		/////////////////////
 		// PROCESSING ITEMS IN A QUEUE
@@ -64,14 +129,24 @@ public class Lecture {
 		System.out.println("       STACKS");
 		System.out.println("####################");
 		System.out.println();
+		
+		Stack<String> browser = new Stack<String>();
+		
 
 		////////////////////
 		// PUSHING ITEMS TO THE STACK
 		////////////////////
-
+		browser.push("google.com");
+		browser.push("amazon.com");
+		browser.push("techelevator.com");
+		
 		////////////////////
 		// POPPING THE STACK
 		////////////////////
 
+		while( !browser.isEmpty() ) {
+			String lastVisitedWebsite = browser.pop();
+			System.out.println("You last visited "+lastVisitedWebsite);
+		}
 	}
 }
