41bfe402f336d31610ce2422bf21d9a8182c39df
Dan Podobnik <daniel.podobnik@gmail.com>
2019-10-07 14:48:42 -0400

    Completed Prime Factors

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataPrimeFactors.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataPrimeFactors.java
@@ -7,6 +7,13 @@ public class KataPrimeFactors {
 	public List<Integer> factorize(int n) {
 		List<Integer> factors = new ArrayList<>();
 		
+		for( int i=2; i <= n; i++ ) {
+			while( n%i == 0 ) {
+				factors.add(i);
+				n /= i;
+			}
+		}
+		
 		return factors;
 	}
 }
