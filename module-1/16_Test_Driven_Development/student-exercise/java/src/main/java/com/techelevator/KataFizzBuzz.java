6cbf0c5e5667e6bc34e2298eade555308a447863
Dan Podobnik <daniel.podobnik@gmail.com>
2019-10-07 13:13:21 -0400

    Didn't read I was supposed to commit between tests, whoops

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -1,5 +1,30 @@
 package com.techelevator;
 
 public class KataFizzBuzz {
-
+	public String fizzBuzz(int number) {
+		String output = "";
+		String input = Integer.toString(number);
+		boolean has5 = input.indexOf('5') >= 0;
+		boolean has3 = input.indexOf('3') >= 0;
+		
+		if( (number%3 == 0 && number > 0) || has3 ) {
+			output += "Fizz";
+		}
+		if( (number%5 == 0 && number > 0) ) {
+			output += "Buzz";
+		}
+		
+		if( has5 ) {
+			output = "Buzz";
+		}
+		if( has3 && has5 ) {
+			output = "FizzBuzz";
+		}
+		
+		if( output.equals("") && number >= 1 && number <= 100 ) {
+			output += number;
+		}
+		
+		return output;
+	}
 }
--- module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -4,5 +4,65 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataFizzBuzzTest {
-
+	KataFizzBuzz kfb;
+	
+	@Before
+	public void setup() {
+		kfb = new KataFizzBuzz();
+	}
+	
+	// Part 1
+	
+	@Test
+	public void test_one() {
+		Assert.assertEquals("1 should return \"1\"", "1", kfb.fizzBuzz(1));
+	}
+	
+	@Test
+	public void test_three() {
+		Assert.assertEquals("3 should return \"Fizz\"", "Fizz", kfb.fizzBuzz(3));
+	}
+	
+	@Test
+	public void test_five() {
+		Assert.assertEquals("5 should return \"Buzz\"", "Buzz", kfb.fizzBuzz(5));
+	}
+	
+	@Test
+	public void test_fifteen() {
+		// commented this out for Part 2, since 15 contains a 5 it will always be "Buzz" even though it's also divisible by 3 (see test_fiftyone)
+		//Assert.assertEquals("15 should return \"FizzBuzz\"", "FizzBuzz", kfb.fizzBuzz(15));
+	}
+	
+	@Test
+	public void test_twentytwo() {
+		Assert.assertEquals("22 should return \"22\"", "22", kfb.fizzBuzz(22));
+	}
+	
+	@Test
+	public void test_zero() {
+		Assert.assertEquals("0 should return \"\"", "", kfb.fizzBuzz(0));
+	}
+	
+	// Part 2
+	
+	@Test
+	public void test_thirteen() {
+		Assert.assertEquals("13 should return \"Fizz\"", "Fizz", kfb.fizzBuzz(13));
+	}
+	
+	@Test
+	public void test_thirtyfive() {
+		Assert.assertEquals("35 should return \"FizzBuzz\"", "FizzBuzz", kfb.fizzBuzz(35));
+	}
+	
+	@Test
+	public void test_fiftyone() {
+		Assert.assertEquals("51 should return \"Buzz\"", "Buzz", kfb.fizzBuzz(51));
+	}
+	
+	@Test
+	public void test_fiftythree() {
+		Assert.assertEquals("53 should return \"FizzBuzz\"", "FizzBuzz", kfb.fizzBuzz(53));
+	}
 }
