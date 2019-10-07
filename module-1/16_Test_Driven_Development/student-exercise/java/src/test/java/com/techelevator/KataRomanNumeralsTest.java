6c2d9c4bf47b3c7ab7d32766e05dc85a4462c50b
Dan Podobnik <daniel.podobnik@gmail.com>
2019-10-07 14:23:33 -0400

    Fixed typos

--- module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataRomanNumeralsTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataRomanNumeralsTest.java
@@ -6,9 +6,9 @@ import static org.junit.Assert.*;
 public class KataRomanNumeralsTest {
 	@Test
 	public final void test_for_0_or_less() {
-		Assert.assertNull(RomanNumeral.convert(0));
-		Assert.assertNull(RomanNumeral.convert(-1));
-		Assert.assertNull(RomanNumeral.convert(Integer.MIN_VALUE));
+		Assert.assertNull(KataRomanNumerals.convert(0));
+		Assert.assertNull(KataRomanNumerals.convert(-1));
+		Assert.assertNull(KataRomanNumerals.convert(Integer.MIN_VALUE));
 	}
 	
 	@Test
@@ -18,7 +18,7 @@ public class KataRomanNumeralsTest {
 		final String expectedString = "I";
 
 		//Act
-		String output = RomanNumeral.convert(oneInt);
+		String output = KataRomanNumerals.convert(oneInt);
         
 		//Assert
 		Assert.assertEquals("One should convert correctly",expectedString, output);
@@ -26,46 +26,46 @@ public class KataRomanNumeralsTest {
 	
 	@Test
 	public final void test_for_low_numbers() {
-		Assert.assertEquals("II", RomanNumeral.convert(2));
-		Assert.assertEquals("III", RomanNumeral.convert(3));
-		Assert.assertEquals("IV", RomanNumeral.convert(4));
-		Assert.assertEquals("V", RomanNumeral.convert(5));
-		Assert.assertEquals("VI", RomanNumeral.convert(6));
-		Assert.assertEquals("VII", RomanNumeral.convert(7));
-		Assert.assertEquals("VIII", RomanNumeral.convert(8));
-		Assert.assertEquals("IX", RomanNumeral.convert(9));
-		Assert.assertEquals("X", RomanNumeral.convert(10));
-		Assert.assertEquals("XI", RomanNumeral.convert(11));
-		Assert.assertEquals("XII", RomanNumeral.convert(12));
-		Assert.assertEquals("XIII", RomanNumeral.convert(13));
-		Assert.assertEquals("XIV", RomanNumeral.convert(14));
-		Assert.assertEquals("XV", RomanNumeral.convert(15));
-		Assert.assertEquals("XVI", RomanNumeral.convert(16));
-		Assert.assertEquals("XVII", RomanNumeral.convert(17));
-		Assert.assertEquals("XVIII", RomanNumeral.convert(18));
-		Assert.assertEquals("XIX", RomanNumeral.convert(19));
-		Assert.assertEquals("XX", RomanNumeral.convert(20));
+		Assert.assertEquals("II", KataRomanNumerals.convert(2));
+		Assert.assertEquals("III", KataRomanNumerals.convert(3));
+		Assert.assertEquals("IV", KataRomanNumerals.convert(4));
+		Assert.assertEquals("V", KataRomanNumerals.convert(5));
+		Assert.assertEquals("VI", KataRomanNumerals.convert(6));
+		Assert.assertEquals("VII", KataRomanNumerals.convert(7));
+		Assert.assertEquals("VIII", KataRomanNumerals.convert(8));
+		Assert.assertEquals("IX", KataRomanNumerals.convert(9));
+		Assert.assertEquals("X", KataRomanNumerals.convert(10));
+		Assert.assertEquals("XI", KataRomanNumerals.convert(11));
+		Assert.assertEquals("XII", KataRomanNumerals.convert(12));
+		Assert.assertEquals("XIII", KataRomanNumerals.convert(13));
+		Assert.assertEquals("XIV", KataRomanNumerals.convert(14));
+		Assert.assertEquals("XV", KataRomanNumerals.convert(15));
+		Assert.assertEquals("XVI", KataRomanNumerals.convert(16));
+		Assert.assertEquals("XVII", KataRomanNumerals.convert(17));
+		Assert.assertEquals("XVIII", KataRomanNumerals.convert(18));
+		Assert.assertEquals("XIX", KataRomanNumerals.convert(19));
+		Assert.assertEquals("XX", KataRomanNumerals.convert(20));
 	}
 	
 	@Test
 	public final void test_for_edge_cases() {
-		Assert.assertEquals("XL", RomanNumeral.convert(40));
-		Assert.assertEquals("XLI", RomanNumeral.convert(41));
-		Assert.assertEquals("XLII", RomanNumeral.convert(42));
-		Assert.assertEquals("XL", RomanNumeral.convert(40));
-		Assert.assertEquals("XLV", RomanNumeral.convert(45));
-		Assert.assertEquals("L", RomanNumeral.convert(50));
-		Assert.assertEquals("XC", RomanNumeral.convert(90));
-		Assert.assertEquals("XCI", RomanNumeral.convert(91));
-		Assert.assertEquals("XCIV", RomanNumeral.convert(94));
-		Assert.assertEquals("C", RomanNumeral.convert(100));
-		Assert.assertEquals("CD", RomanNumeral.convert(400));
-		Assert.assertEquals("CDX", RomanNumeral.convert(410));
-		Assert.assertEquals("D", RomanNumeral.convert(500));
-		Assert.assertEquals("DVI", RomanNumeral.convert(506));
-		Assert.assertEquals("CM", RomanNumeral.convert(900));
-		Assert.assertEquals("CML", RomanNumeral.convert(950));
-		Assert.assertEquals("M", RomanNumeral.convert(1000));
-		Assert.assertEquals("MMXIX", RomanNumeral.convert(2019));
+		Assert.assertEquals("XL", KataRomanNumerals.convert(40));
+		Assert.assertEquals("XLI", KataRomanNumerals.convert(41));
+		Assert.assertEquals("XLII", KataRomanNumerals.convert(42));
+		Assert.assertEquals("XL", KataRomanNumerals.convert(40));
+		Assert.assertEquals("XLV", KataRomanNumerals.convert(45));
+		Assert.assertEquals("L", KataRomanNumerals.convert(50));
+		Assert.assertEquals("XC", KataRomanNumerals.convert(90));
+		Assert.assertEquals("XCI", KataRomanNumerals.convert(91));
+		Assert.assertEquals("XCIV", KataRomanNumerals.convert(94));
+		Assert.assertEquals("C", KataRomanNumerals.convert(100));
+		Assert.assertEquals("CD", KataRomanNumerals.convert(400));
+		Assert.assertEquals("CDX", KataRomanNumerals.convert(410));
+		Assert.assertEquals("D", KataRomanNumerals.convert(500));
+		Assert.assertEquals("DVI", KataRomanNumerals.convert(506));
+		Assert.assertEquals("CM", KataRomanNumerals.convert(900));
+		Assert.assertEquals("CML", KataRomanNumerals.convert(950));
+		Assert.assertEquals("M", KataRomanNumerals.convert(1000));
+		Assert.assertEquals("MMXIX", KataRomanNumerals.convert(2019));
 	}
 }
