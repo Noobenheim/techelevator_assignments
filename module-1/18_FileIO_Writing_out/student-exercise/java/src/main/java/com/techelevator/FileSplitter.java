a6f6f9eefd9cc3131a5cb33522cd56d53f6a0abc
Dan Podobnik <daniel.podobnik@gmail.com>
2019-10-09 20:22:36 -0400

    Completed both assignments

--- module-1/18_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FileSplitter.java
+++ module-1/18_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FileSplitter.java
@@ -1,9 +1,71 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+//Where is the input file (please include the path to the file)? [path-to-file]/input.txt
+//How many lines of text (max) should there be in the split files? 3
+//The input file has 50 lines of text.
+
 public class FileSplitter {
-
 	public static void main(String[] args) {
-
+		Scanner inputScanner = new Scanner(System.in);
+		File inputFile;
+		int maxLines = 0;
+		
+		do {
+			System.out.print("Where is the input file (please include the path to the file)? ");
+			inputFile = new File(inputScanner.nextLine());
+			
+			if( !inputFile.exists() ) {
+				System.out.println("Source file does not exist.");
+			}
+		} while( !inputFile.exists() );
+		
+		boolean validInput = false;
+		do {
+			System.out.print("How many lines of text (max) should there be in the split files? ");
+			String input = inputScanner.nextLine();
+			
+			try {
+				maxLines = Integer.parseInt(input);
+				validInput = true;
+			} catch( NumberFormatException e ) {
+				System.out.println("Please enter a valid number");
+			}
+		} while( !validInput );
+		
+		String ext;
+		String name;
+		if( inputFile.getName().indexOf('.') >= 0 ) {
+			ext = inputFile.getName().substring(inputFile.getName().lastIndexOf('.'));
+			name = inputFile.getName().substring(0, inputFile.getName().lastIndexOf('.'));
+		} else {
+			ext = "";
+			name = inputFile.getName();
+		}
+		
+		int lineCount = 0;
+		try(Scanner fileScanner = new Scanner(inputFile) ) {
+			for( long i=1; fileScanner.hasNextLine() && i < Long.MAX_VALUE; i++ ) {
+				File outputFile = new File(name+"-"+i+ext);
+				try( PrintWriter writer = new PrintWriter(new FileWriter(outputFile)) ) {
+					for( int j = 0; j < maxLines && fileScanner.hasNextLine(); j++ ) {
+						lineCount++;
+						writer.println(fileScanner.nextLine());
+					}
+				} catch( IOException e ) {
+					System.out.println("Error when writing to file.");
+				}
+			}
+		} catch( IOException e ) {
+			System.out.println("Unable to read file.");
+		}
+		System.out.println("The input file has "+lineCount+" lines of text.");
+		
+		inputScanner.close();
 	}
-
 }
--- module-1/18_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/18_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,39 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class FizzWriter {
 
 	public static void main(String[] args) {
-
+		String destinationFileName = "FizzBuzz.txt";
+		
+		File destinationFile = new File(destinationFileName);
+		
+		try( PrintWriter writer = new PrintWriter(new FileWriter(destinationFile))) {
+			for( int i = 1; i <= 300; i++ ) {
+				String println;
+				
+				if( i%3 == 0 && i%5 == 0 ) {
+					println = "FizzBuzz";
+				} else if( i%3 == 0 || Integer.toString(i).contains("3") ) {
+					println = "Fizz";
+				} else if( i%5 == 0 || Integer.toString(i).contains("5") ) {
+					println = "Buzz";
+				} else {
+					println = Integer.toString(i);
+				}
+				
+				writer.println(println);
+			}
+			
+			System.out.println("FizzBuzz.txt has been created.");
+		} catch( IOException e ) {
+			System.err.println("Failed to write to file.");
+			System.exit(1);
+		}
 	}
 
 }
